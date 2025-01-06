9bfb3f4be27f281bee7ab8fe2a7e433aacb2c2e4
Vladimir Efimov <vladimir.a.efimov@gmail.com>
2025-01-06 19:12:08 +0700

    Setup JUnit 5

---
+++ src/test/java/ru/javawebinar/topjavagraduation/BaseTest.java
@@ -0,0 +1,11 @@
+package ru.javawebinar.topjavagraduation;
+
+import org.junit.jupiter.api.Test;
+
+public class BaseTest {
+
+    @Test
+    void test() {
+        System.out.println("PASSED");
+    }
+}
