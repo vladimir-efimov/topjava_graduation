98dbe8d4fe203cb72e6ec6525c670299453f8d4c
Vladimir Efimov <vladimir.a.efimov@gmail.com>
2025-01-08 22:13:56 +0700

    Implement findByEmail for UserRepository

--- src/test/java/ru/javawebinar/topjavagraduation/repository/InMemoryUserRepository.java
+++ src/test/java/ru/javawebinar/topjavagraduation/repository/InMemoryUserRepository.java
@@ -2,5 +2,12 @@ package ru.javawebinar.topjavagraduation.repository;
 
 import ru.javawebinar.topjavagraduation.model.User;
 
+import java.util.Optional;
+
 public class InMemoryUserRepository extends InMemoryManagedEntityRepository<User> implements UserRepository {
+    public Optional<User> findByEmail(String email) {
+        return entities.values().stream()
+                .filter(e -> e.getEmail().equals(email))
+                .findFirst();
+    }
 }
