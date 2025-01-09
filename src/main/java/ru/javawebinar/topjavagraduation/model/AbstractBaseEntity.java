7046ec20a1fb891776e00c58f712fbabd10e52fc
Vladimir Efimov <vladimir.a.efimov@gmail.com>
2025-01-09 17:54:53 +0700

    Override equals() and hashCode() for future use of ORM

--- src/main/java/ru/javawebinar/topjavagraduation/model/AbstractBaseEntity.java
+++ src/main/java/ru/javawebinar/topjavagraduation/model/AbstractBaseEntity.java
@@ -27,4 +27,29 @@ public abstract class AbstractBaseEntity implements Cloneable {
     public AbstractBaseEntity clone() throws CloneNotSupportedException {
         return (AbstractBaseEntity) super.clone();
     }
+
+    /* todo: consider using business-model comparison instead of DAO-style comparison based on id.
+       The problem with id-based comparison is that
+       - in-memory repository can't utilize hash code for preventing unique constraint violations
+       - tests can't rely on equals(), tests will use matchers
+       Need to check how equals and hashCode is used by ORM. After that following approach may be
+       tested:
+       - hashCode() is be defined on fields composing unique constraint
+       - equals() compares fields bypassing comparison of null id with not-null id
+     */
+
+    //todo: substitution getClass() when plug-in ORM, need take into account proxy classes and lazy initialization
+    //https://jpa-buddy.com/blog/hopefully-the-final-article-about-equals-and-hashcode-for-jpa-entities-with-db-generated-ids/
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        return id != null && id == ((AbstractBaseEntity) o).getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
