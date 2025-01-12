756e2b21ceb3750fb3e4dfe520cf1410f3117d2b
Vladimir Efimov <vladimir.a.efimov@gmail.com>
2025-01-13 00:46:38 +0700

    Add operation restriction checks for VoteService

--- src/main/java/ru/javawebinar/topjavagraduation/service/VoteService.java
+++ src/main/java/ru/javawebinar/topjavagraduation/service/VoteService.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public class VoteService extends AbstractBaseEntityService<Vote> {
 
+    private final static int END_VOTE_HOURS = 11;
     private final VoteRepository repository;
 
     public VoteService(VoteRepository repository) {
@@ -19,7 +20,46 @@ public class VoteService extends AbstractBaseEntityService<Vote> {
         return repository.findByUser(id);
     }
 
-    public List<Vote> findDate(Date date) {
+    public List<Vote> findByDate(Date date) {
         return repository.findByDate(date);
+    }
+
+    @Override
+    protected void validateOnCreate(Vote vote) {
+        super.validateOnCreate(vote);
+        assertVoteOperationIsAllowed(vote);
+    }
+
+    @Override
+    protected void validateOnUpdate(Vote vote) {
+        super.validateOnUpdate(vote);
+        assertVoteOperationIsAllowed(vote);
+    }
+
+    @Override
+    protected void validateOnDelete(Vote vote) {
+        super.validateOnDelete(vote);
+        assertVoteOperationIsAllowed(vote);
+    }
+
+    private void assertVoteOperationIsAllowed(Vote vote) {
+        Date currentDate = getCurrentDate();
+        if (currentDate.getHours() >= END_VOTE_HOURS) {
+            throw new IllegalStateException("Operations with vote are not allowed after " + END_VOTE_HOURS + " hours");
+        }
+        if(vote.getDate() == null) {
+            throw new IllegalArgumentException("Vote should has not null date");
+        }
+        currentDate.setHours(0);
+        currentDate.setMinutes(0);
+        currentDate.setSeconds(0);
+        if(vote.getDate().before(currentDate)) {
+            throw new IllegalStateException("Can't operate with date in the past");
+        }
+    }
+
+    // todo: need some way to overwrite current date in tests
+    Date getCurrentDate() {
+        return new Date(System.currentTimeMillis());
     }
 }
