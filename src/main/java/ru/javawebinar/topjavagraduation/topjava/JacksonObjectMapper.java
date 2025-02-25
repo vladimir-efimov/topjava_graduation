59686ed646b261096787cbc4fafa47ed24fedaa0
Vladimir Efimov <vladimir.a.efimov@gmail.com>
2025-02-25 11:01:41 +0700

    Use fasterxml.jackson.datatype to fix problem with converting Hibebernate proxy classes to JSON

---
+++ src/main/java/ru/javawebinar/topjavagraduation/topjava/JacksonObjectMapper.java
@@ -0,0 +1,39 @@
+package ru.javawebinar.topjavagraduation.topjava;
+
+// Code from TopJava course (https://github.com/JavaWebinar/topjava) with some modifications
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+/**
+ * <p>
+ * Handling Hibernate lazy-loading
+ *
+ * @link https://github.com/FasterXML/jackson
+ * @link https://github.com/FasterXML/jackson-datatype-hibernate
+ * @link https://github.com/FasterXML/jackson-docs/wiki/JacksonHowToCustomSerializers
+ */
+public class JacksonObjectMapper extends ObjectMapper {
+
+    private static final ObjectMapper MAPPER = new JacksonObjectMapper();
+
+    private JacksonObjectMapper() {
+        registerModule(new Hibernate6Module());
+
+        registerModule(new JavaTimeModule());
+        configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+
+        setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE);
+        setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+        setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    }
+
+    public static ObjectMapper getMapper() {
+        return MAPPER;
+    }
+}
