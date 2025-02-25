bc98e5a27492608b1196eaf92eccf1d6c0c8eece
Vladimir Efimov <vladimir.a.efimov@gmail.com>
2025-02-25 10:57:19 +0700

    JpaMealRepository: fixed issue with attempt to persist detached entity

--- src/main/java/ru/javawebinar/topjavagraduation/repository/datajpa/JpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjavagraduation/repository/datajpa/JpaMealRepository.java
@@ -1,7 +1,11 @@
 package ru.javawebinar.topjavagraduation.repository.datajpa;
 
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjavagraduation.model.Meal;
+import ru.javawebinar.topjavagraduation.model.Restaurant;
 import ru.javawebinar.topjavagraduation.repository.MealRepository;
 
 import java.util.List;
@@ -10,6 +14,8 @@ import java.util.List;
 public class JpaMealRepository extends JpaManagedEntityRepository<Meal> implements MealRepository {
 
     private final IJpaMealRepository repository;
+    @Autowired
+    EntityManager em;
 
     public JpaMealRepository(IJpaMealRepository repository) {
         super(repository);
@@ -19,5 +25,13 @@ public class JpaMealRepository extends JpaManagedEntityRepository<Meal> implemen
     @Override
     public List<Meal> findByRestaurant(int id) {
         return repository.findByRestaurant(id);
+    }
+
+    @Override
+    @Transactional
+    public Meal save(Meal meal) {
+        Meal mealCopy = new Meal(meal.getId(), meal.isEnabled(), meal.getName(),meal.getPrice(),
+                em.getReference(Restaurant.class, meal.getRestaurant().getId()));
+        return repository.save(mealCopy);
     }
 }
