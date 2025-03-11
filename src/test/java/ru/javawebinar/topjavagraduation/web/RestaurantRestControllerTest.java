311f07ab7c40cc524cac35c08a37535093c3dd08
Vladimir Efimov <vladimir.a.efimov@gmail.com>
2025-03-11 12:41:47 +0700

    Add test for RestaurantRestController

---
+++ src/test/java/ru/javawebinar/topjavagraduation/web/RestaurantRestControllerTest.java
@@ -0,0 +1,54 @@
+package ru.javawebinar.topjavagraduation.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import ru.javawebinar.topjavagraduation.data.TestDataProvider;
+import ru.javawebinar.topjavagraduation.model.Restaurant;
+import ru.javawebinar.topjavagraduation.service.RestaurantService;
+import ru.javawebinar.topjavagraduation.validation.exception.ErrorInfo;
+import ru.javawebinar.topjavagraduation.validation.exception.ErrorType;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.javawebinar.topjavagraduation.data.TestData.newRestaurant;
+import static ru.javawebinar.topjavagraduation.web.RestaurantRestController.REST_URL;
+
+public class RestaurantRestControllerTest extends AbstractRestControllerTest {
+
+    @Autowired
+    private TestDataProvider<Restaurant> testDataProvider;
+
+    @Autowired
+    private RestaurantService service;
+
+    @Test
+    void createWithLocation() throws Exception {
+        ResultActions action = mockMvc.perform(MockMvcRequestBuilders.post(REST_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(MAPPER.writeValueAsString(newRestaurant)))
+                .andDo(print())
+                .andExpect(status().isCreated());
+        Restaurant receivedRestaurant = MAPPER.readValue(action.andReturn().getResponse().getContentAsString(), Restaurant.class);
+        testDataProvider.getMatcher().assertMatch(newRestaurant, receivedRestaurant);
+    }
+
+    @Test
+    void tryCreateDuplicated() throws Exception {
+        var restaurant = new Restaurant("cafe_dupl", "address_dupl");
+        service.create((Restaurant) restaurant.clone());
+
+        ResultActions result = mockMvc.perform(MockMvcRequestBuilders.post(RestaurantRestController.REST_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(MAPPER.writeValueAsString(restaurant)))
+                .andDo(print())
+                .andExpect(status().isConflict());
+        ErrorInfo error = MAPPER.readValue(result.andReturn().getResponse().getContentAsString(), ErrorInfo.class);
+        assertEquals(ErrorType.DATA_CONFLICT_ERROR, error.getErrorType());
+        assertEquals(1, error.getDetails().length);
+        assertEquals("Restaurant with the same name and address already exists", error.getDetails()[0]);
+    }
+}
